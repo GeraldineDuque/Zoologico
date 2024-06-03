63ce65ba80851a98384e9763ddc14714a542ecf8
unknown <gospina@cesde.net>
2024-06-03 17:50:55 -0500

    Guardando

---
+++ src/Familia.java
@@ -0,0 +1,50 @@
+package src;
+
+public class Familia{
+
+    private Animal papa= new Animal();
+    private Animal mama= new Animal();
+    private Animal hijo = new Animal();
+
+    public Familia(Animal papa, Animal mama) {
+        this.papa = papa;
+        this.mama = mama;
+    }
+
+    public void imprimirFamilia(){
+
+        //Este metodo muestra los nombres de los integrantes de la familia
+        System.out.println(
+                "El papá es: "+this.papa.getNombre()+
+                        "\nLa mamá es: "+this.mama.getNombre()+
+                        "\nEl hijo es: "+this.hijo.getNombre()
+        );
+
+    }
+
+    public void tenerHijo(String nombre){
+
+        if(this.papa.getPareja()!=null){
+
+            Animal hijo= new Animal();
+            hijo.setNombre(nombre);
+            hijo.setPeso(1.0);
+            double random = Math.random();
+            if(random<0.5){
+                hijo.setGenero("F");
+            }else{
+                hijo.setGenero("M");
+            }
+            this.hijo=hijo;
+
+        }else{
+            System.out.println("Aun no puedes tener hijos");
+        }
+
+
+    }
+
+
+
+
+}
